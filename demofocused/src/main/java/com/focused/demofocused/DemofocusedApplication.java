2186cef4d6577af95616e29f6a20369a0bf5c4ca
klamsal1 <kadarlamsal1@gmail.com>
2025-01-29 19:35:40 -0600

    my learning phase

--- demofocused/src/main/java/com/focused/demofocused/DemofocusedApplication.java
+++ demofocused/src/main/java/com/focused/demofocused/DemofocusedApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemofocusedApplication {
 
+
 	public static void main(String[] args) {
 		SpringApplication.run(DemofocusedApplication.class, args);
 	}
